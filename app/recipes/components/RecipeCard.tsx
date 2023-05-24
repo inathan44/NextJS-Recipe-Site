63dde2bca1eefa107a440f1b42499285f5d7b47c
inathan44 <inathan44@yahoo.com>
2023-05-24 11:47:49 -0400

    Move to diff component folder

--- app/recipes/components/RecipeCard.tsx
+++ app/recipes/components/RecipeCard.tsx
@@ -9,24 +9,25 @@ export type RecipeCardProps = {
 export default function RecipeCard(props: RecipeCardProps) {
   const { recipe } = props;
   return (
-    <div className='flex h-[500px] flex-col p-2 transition-all hover:bg-gray-100'>
+    <div className='max-w-96 flex h-[500px] w-full flex-col p-3 transition-all hover:bg-yellow-500'>
       {recipe.images && recipe.images.length > 0 && (
         <Image
           src={recipe.images[0]}
           alt={`Photo of ${recipe.name}`}
-          width={384}
+          width={100}
           height={100}
+          className='w-full'
         />
       )}
-      <div className='flex w-96 grow flex-col gap-6'>
-        {/* <p className='text-sm text-gray-500'>{recipe.tags.join(', ')}</p> */}
-        <h2 className='text-center text-2xl font-bold'>{recipe.name}</h2>
+      <p className='text-xs italic text-gray-500'>{recipe.tags.join(', ')}</p>
+      <div className='flex w-full grow flex-col gap-6'>
+        <h2 className=' text-2xl font-bold'>{recipe.name}</h2>
         <div className='grow'>
           <p className='text-lg'>{recipe.description}</p>
         </div>
         <Link href={`recipes/${recipe.id}`}>
-          <p className='inline rounded-full bg-primary-dark px-4 py-2 text-primary-light hover:bg-darker-dark'>
-            View Recipe -{`>`}
+          <p className='bg-primary-dark px-4 py-2 text-center text-primary-light hover:bg-darker-dark'>
+            View Recipe &rarr;
           </p>
         </Link>
       </div>
