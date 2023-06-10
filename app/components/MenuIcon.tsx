6eb60a4b60764e38ac31a2c74e4612e8638f2c32
inathan44 <inathan44@yahoo.com>
2023-06-10 14:03:03 -0400

    Icons

---
+++ app/components/MenuIcon.tsx
@@ -0,0 +1,22 @@
+const MenuIcon = () => {
+  return (
+    <div className='rounded-full bg-darker-light p-1'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='h-7 w-7'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M3.75 9h16.5m-16.5 6.75h16.5'
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default MenuIcon;
