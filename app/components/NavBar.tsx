79be7de4286fb5bc4bb0f0452355f3eb693a6f06
inathan44 <inathan44@yahoo.com>
2023-06-11 00:46:14 -0400

    Added mobile nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
-import MenuIcon from './MenuIcon';
+import MobileNav from './MobileNav';
 
 export default function NavBar() {
   return (
-    <nav className='flex items-center justify-between p-3'>
-      <Link href={'/'}>
-        <h1 className='text-3xl'>
-          CLEAN<span className='text-slate-700'>PL8.</span>
-        </h1>
-      </Link>
-      <div>
-        <Link
-          className='hidden border-darker-dark text-xl font-bold hover:border-b-2 md:block'
-          href={'recipes'}
-        >
-          Recipes
+    <header className='sticky top-0 z-10 bg-white'>
+      <nav className='flex h-16 items-center justify-between px-6'>
+        <Link href={'/'}>
+          <h1 className='text-3xl'>
+            CLEAN<span className='text-slate-700'>PL8.</span>
+          </h1>
         </Link>
-      </div>
-      <div className='md:hidden'>
-        <MenuIcon />
-      </div>
-    </nav>
+        <div>
+          <Link
+            className='hidden border-darker-dark text-xl font-bold hover:border-b-2 md:block'
+            href={'recipes'}
+          >
+            Recipes
+          </Link>
+        </div>
+        <MobileNav />
+      </nav>
+    </header>
   );
 }
