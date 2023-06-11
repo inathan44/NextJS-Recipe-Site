874c7fb8de6c55f87bd9670bcf381acdf25a47b4
inathan44 <inathan44@yahoo.com>
2023-06-11 00:45:35 -0400

    Mobile footer styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className='bg-darker-dark p-4 text-primary-light'>
-      <div className='flex flex-col items-center justify-between md:flex-row md:items-start'>
+      <div className='flex flex-col-reverse items-center justify-between gap-6 md:flex-row md:items-start'>
         <Link href={'/'} className='text-3xl text-primary-light'>
           CLEAN<span className='text-lighter-light'>PL8.</span>
         </Link>
-        <ul className='flex flex-col gap-12 text-xl md:flex-row'>
+        <ul className='flex w-full justify-between text-center md:flex-row md:gap-12'>
           <li>
             <Link href={'/about'}>About</Link>
           </li>
@@ -22,7 +22,7 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <p className='mt-4 text-right text-sm'>
+      <p className='mt-2 text-center text-sm md:text-right'>
         &copy; {new Date().getFullYear()} Nathan {`Bro's`}
       </p>
     </footer>
