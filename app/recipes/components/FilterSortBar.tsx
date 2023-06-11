7719caa9c1d182395c4683f08e6bc875b6b7916f
inathan44 <inathan44@yahoo.com>
2023-06-11 00:46:24 -0400

    Filter sort section work in progress

--- app/recipes/components/FilterSortBar.tsx
+++ app/recipes/components/FilterSortBar.tsx
@@ -1,11 +1,18 @@
+import SearchIcon from '@/app/components/SearchIcon';
+import FiltersIcon from '@/app/components/FiltersIcon';
+
 const FilterSortBar = () => {
   return (
-    <div className='border-y border-darker-dark'>
-      <ul className='flex flex-col justify-center gap-20 py-6 text-2xl sm:flex-row'>
-        <li>Meal</li>
-        <li>Category</li>
-        <li>Sort</li>
-      </ul>
+    <div className='mx-6 mt-12 border-darker-dark'>
+      <div className='flex items-center gap-4 border-b border-darker-dark pb-2'>
+        <SearchIcon />
+        <input
+          type='text'
+          className='w-full bg-transparent active:border-transparent'
+          placeholder='Search Recipes'
+        />
+        <FiltersIcon />
+      </div>
     </div>
   );
 };
