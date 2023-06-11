d4687744df1c03b55493f80f072244484bffdecd
inathan44 <inathan44@yahoo.com>
2023-06-10 22:48:04 -0400

    Updated edit/add functions

--- app/utils/documentFunctions.ts
+++ app/utils/documentFunctions.ts
@@ -10,43 +10,25 @@ export async function createRecipeDoc(
   setImageUrl: React.Dispatch<React.SetStateAction<string>>,
   setAddDocLoading: React.Dispatch<React.SetStateAction<boolean>>,
   setAddDocError: React.Dispatch<React.SetStateAction<string>>,
-  data: RecipeSchema
+  data: RecipeSchema,
+  reset: any
 ) {
+  setAddDocLoading(true);
+
+  // Checking type validity
+  const recipeToAdd: Recipe = {
+    name: data.name,
+    ingredients: data.ingredients,
+    directions: data.directions.map(({ direction }) => direction),
+    image: imageUrl,
+    tags: data.tags || [],
+    description: data.description,
+    cookTime: data.cookTime,
+    prepTime: data.prepTime,
+    servings: data.servings,
+  };
+
   try {
-    alert('Im working omg yeah!');
-
-    setAddDocLoading(true);
-    // Change ingredients to correct format
-    // Change ingredients to correct format
-    // const newIngredients = ingredients.reduce(
-    //   (acc: Ingredient[], cv: string, idx: number) => {
-    //     acc.push({
-    //       name: cv,
-    //       amount: +amounts[idx],
-    //       measurement: measurements[idx],
-    //     });
-    //     return acc;
-    //   },
-    //   []
-    // );
-
-    // Checking type validity
-    const recipeToAdd: Recipe = {
-      name: data.name,
-      ingredients: data.ingredients,
-      directions: data.directions.map(({ direction }) => direction),
-      image: imageUrl,
-      tags: data.tags || [],
-      description: data.description,
-      cookTime: data.cookTime,
-      prepTime: data.prepTime,
-      servings: data.servings,
-    };
-
-    // let recipeWithoutUndefined = Object.keys(recipeToAdd)
-    //   .filter((k) => recipeToAdd[k as keyof Recipe] != null)
-    //   .reduce((a, k) => ({ ...a, [k]: recipeToAdd[k as keyof Recipe] }), {});
-
     await addDoc(recipeRef, recipeToAdd);
 
     setImageUrl('');
@@ -54,14 +36,36 @@ export async function createRecipeDoc(
     setAddDocLoading(false);
     const fileInput: any = document.getElementById('images');
     fileInput.value = null;
-  } catch (e: any) {
-    setAddDocError(e.message);
-    setAddDocLoading(false);
-    console.log('Adding recipe doc error:', e);
+
+    reset();
+  } catch (e) {
+    console.log(e);
   }
 }
 
 export async function uploadImages(
+  image: File,
+  setImageUrl: React.Dispatch<React.SetStateAction<string>>,
+  setUploadLoading: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  if (!image) return;
+
+  setUploadLoading(true);
+
+  const storageRef = ref(storage, `${image.name + v4()}`);
+
+  // Upload the images to Firebase Storage
+  const snapshot = await uploadBytesResumable(storageRef, image);
+
+  // Get the download URL of the uploaded images
+  const downloadUrl = await getDownloadURL(snapshot.ref);
+
+  setUploadLoading(false);
+  // Update the state with the download URL
+  setImageUrl(downloadUrl);
+}
+
+export async function editImages(
   image: File,
   setImageUrl: React.Dispatch<React.SetStateAction<string>>,
   setUploadLoading: React.Dispatch<React.SetStateAction<boolean>>
@@ -87,141 +91,32 @@ export async function uploadImages(
   }
 }
 
-export async function editImages(
-  image: File,
-  setImageUrl: React.Dispatch<React.SetStateAction<string>>,
-  setUploadLoading: React.Dispatch<React.SetStateAction<boolean>>,
-  imageUrl: string
-) {
-  if (!image) return;
-
-  setUploadLoading(true);
-  try {
-    const storageRef = ref(storage, `${image.name + v4()}`);
-
-    // Upload the images to Firebase Storage
-    const snapshot = await uploadBytesResumable(storageRef, image);
-
-    // Get the download URL of the uploaded images
-    const downloadUrl = await getDownloadURL(snapshot.ref);
-
-    setUploadLoading(false);
-    // Update the state with the download URL
-    setImageUrl(downloadUrl);
-  } catch (e: any) {
-    setUploadLoading(false);
-    console.log(e);
-  }
-}
-
-export function handleDirectionsChange(
-  e: React.ChangeEvent<HTMLInputElement>,
-  idx: number,
-  directions: string[],
-  setDirections: React.Dispatch<React.SetStateAction<string[]>>
-) {
-  let data = [...directions];
-
-  data[idx] = e.target.value;
-
-  setDirections(data);
-}
-
-export function handleIngredientsChange(
-  e: React.ChangeEvent<HTMLInputElement>,
-  idx: number,
-  ingredients: string[],
-  setIngredients: React.Dispatch<React.SetStateAction<string[]>>
-) {
-  let data = [...ingredients];
-
-  data[idx] = e.target.value;
-
-  setIngredients(data);
-}
-
-export function handleMeasurementsChange(
-  e: React.ChangeEvent<HTMLInputElement>,
-  idx: number,
-  measurements: string[],
-  setMeasurements: React.Dispatch<React.SetStateAction<string[]>>
-) {
-  let data = [...measurements];
-
-  data[idx] = e.target.value;
-
-  setMeasurements(data);
-}
-
-export function handleAmountsChange(
-  e: React.ChangeEvent<HTMLInputElement>,
-  idx: number,
-  amounts: string[],
-  setAmounts: React.Dispatch<React.SetStateAction<string[]>>
-) {
-  let data = [...amounts];
-
-  data[idx] = e.target.value;
-
-  setAmounts(data);
-}
-
-export function addDirection(
-  setDirections: React.Dispatch<React.SetStateAction<string[]>>
-) {
-  setDirections((prev) => [...prev, '']);
-}
-
-export function addIngredient(
-  setIngredients: React.Dispatch<React.SetStateAction<string[]>>
-): void {
-  setIngredients((prev) => [...prev, '']);
-}
-
 export async function updateRecipe(
-  recipeId: string,
-  name: string,
-  ingredients: string[],
-  directions: string[],
   imageUrl: string,
-  tags: string,
-  description: string,
-  amounts: string[],
-  measurements: string[]
+  setAddDocLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  setAddDocError: React.Dispatch<React.SetStateAction<string>>,
+  data: RecipeSchema,
+  recipeId: string
 ) {
   const recipeRef = doc(db, 'recipes', recipeId);
 
-  // Change ingredients to correct format
-  const newIngredients = ingredients.reduce((acc: Ingredient[], cv, idx) => {
-    acc.push({
-      name: cv.toLowerCase(),
-      amount: +amounts[idx],
-      measurement: measurements[idx].toLowerCase(),
-    });
-    return acc;
-  }, []);
-
-  let newTags;
-  newTags = tags.split(/,\s*|,/);
-
   // Checking type validity
   const recipeToAdd: Recipe = {
-    name: name.toLowerCase(),
-    ingredients: newIngredients,
-    directions,
+    name: data.name,
+    ingredients: data.ingredients,
+    directions: data.directions.map(({ direction }) => direction),
     image: imageUrl,
-    tags: newTags,
-    description,
+    tags: data.tags || [],
+    description: data.description,
+    cookTime: data.cookTime,
+    prepTime: data.prepTime,
+    servings: data.servings,
   };
 
-  let recipeWithoutUndefined = Object.keys(recipeToAdd)
-    .filter((k) => recipeToAdd[k as keyof Recipe] != null)
-    .reduce((a, k) => ({ ...a, [k]: recipeToAdd[k as keyof Recipe] }), {});
-
   try {
-    await updateDoc(recipeRef, recipeWithoutUndefined);
-  } catch (e: any) {
-    console.log(e.message);
+    await updateDoc(recipeRef, recipeToAdd);
+  } catch (e) {
+    console.log(e);
   }
 }
 
