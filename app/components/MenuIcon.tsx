faba80b7f7ad55b17d41208f3f9fc2605748aabd
inathan44 <inathan44@yahoo.com>
2023-06-11 00:46:06 -0400

    Removed bg

--- app/components/MenuIcon.tsx
+++ app/components/MenuIcon.tsx
@@ -1,6 +1,6 @@
 const MenuIcon = () => {
   return (
-    <div className='rounded-full bg-darker-light p-1'>
+    <div className='rounded-full  p-1'>
       <svg
         xmlns='http://www.w3.org/2000/svg'
         fill='none'
