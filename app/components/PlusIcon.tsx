d3b1e52d01d4f9e686edd3488af22867800df9ad
inathan44 <inathan44@yahoo.com>
2023-06-16 00:53:00 -0400

    Icons

---
+++ app/components/PlusIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function PlusIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='h-5 w-5 stroke-primary-light dark:stroke-primary-dark'
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M12 6v12m6-6H6' />
+    </svg>
+  );
+}
