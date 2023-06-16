0b0d04cc0f1c263ad79abad170d78979f233f4b1
inathan44 <inathan44@yahoo.com>
2023-06-16 19:21:04 -0400

    Formatting

--- app/recipes/components/AddEditForm.tsx
+++ app/recipes/components/AddEditForm.tsx
@@ -188,121 +188,88 @@ export default function AddEditForm({ recipeId, type }: Params) {
   console.log('addDocLoading', addDocLoading);
 
   return (
-    <div className=' px-4'>
-      <div className='flex justify-center pt-5'>
-        {imageUrl && (
-          <button
-            onClick={() => setImageUrl('')}
-            className='mx-auto hover:bg-red-300'
-          >
-            <div className='max-w-sm'>
-              <Image
-                src={imageUrl}
-                alt='Recipe Photo'
-                height={1920}
-                width={1080}
-                priority={true}
-                className='overflow-hidden rounded-xl hover:opacity-30'
-              />
-            </div>
-          </button>
-        )}
-      </div>
+    <>
+      <div className=' px-4'>
+        <div className='flex justify-center pt-5'>
+          {imageUrl && (
+            <button
+              onClick={() => setImageUrl('')}
+              className='mx-auto hover:bg-red-300'
+            >
+              <div className='max-w-sm'>
+                <Image
+                  src={imageUrl}
+                  alt='Recipe Photo'
+                  height={1920}
+                  width={1080}
+                  priority={true}
+                  className='overflow-hidden rounded-xl hover:opacity-30'
+                />
+              </div>
+            </button>
+          )}
+        </div>
 
-      <div className='mx-auto mt-8 max-w-md'>
-        <form onSubmit={handleSubmit(onSubmit)} className='text-primary-dark'>
-          <label
-            className='mt-4 block text-lg font-semibold dark:text-primary-light'
-            htmlFor='name'
-          >
-            Recipe Name <span className='text-red-500'>*</span>
-            <span className='text-sm font-light text-red-400'>
-              {errors.name?.message}
-            </span>
-          </label>
-          <input
-            {...register('name')}
-            className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
-            type='text'
-          />
+        <div className='mx-auto mt-8 max-w-md'>
+          <form onSubmit={handleSubmit(onSubmit)} className='text-primary-dark'>
+            <label
+              className='mt-4 block text-lg font-semibold dark:text-primary-light'
+              htmlFor='name'
+            >
+              Recipe Name <span className='text-red-500'>*</span>
+              <span className='text-sm font-light text-red-400'>
+                {errors.name?.message}
+              </span>
+            </label>
+            <input
+              {...register('name')}
+              className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
+              type='text'
+            />
 
-          {/* Ingredients Section */}
-          <div className='mt-6'>
-            <div className='mt-1 flex items-center gap-5'>
-              <h1 className='block text-lg font-semibold dark:text-primary-light'>
-                Ingredients <span className='text-red-500'>*</span>
-              </h1>
-              <button
-                className='dark flex w-16 items-center justify-between rounded-full px-2 dark:bg-primary-light dark:text-primary-dark'
-                type='button'
-                onClick={() =>
-                  ingredientAppend({ name: '', measurement: '', amount: 0 })
-                }
-              >
-                <p>Add</p>
-                <PlusIcon />
-              </button>
-            </div>
-            <div className='flex dark:text-primary-light'>
-              <p className='w-full'>Name</p>
-              <p className='w-full'>Unit</p>
-              <p className='w-full'>Amount</p>
-            </div>
-            <div className='flex flex-col gap-2'>
-              {ingredientFields.map((field, idx) => (
-                <div className='flex gap-3' key={field.id}>
-                  <input
-                    placeholder={`Olive oil`}
-                    className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
-                    {...register(`ingredients.${idx}.name`)}
-                  />
-                  <input
-                    placeholder={`tablespoon`}
-                    className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
-                    {...register(`ingredients.${idx}.measurement`)}
-                  />
-                  <input
-                    placeholder={`2`}
-                    className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
-                    {...register(`ingredients.${idx}.amount`)}
-                  />
-                  <button
-                    type='button'
-                    onClick={() => ingredientRemove(idx)}
-                    className='rounded bg-red-500'
-                  >
-                    <XIcon />
-                  </button>
-                </div>
-              ))}
-            </div>
-
-            {/* Direction section */}
+            {/* Ingredients Section */}
             <div className='mt-6'>
               <div className='mt-1 flex items-center gap-5'>
                 <h1 className='block text-lg font-semibold dark:text-primary-light'>
-                  Directions <span className='text-red-500'>*</span>
+                  Ingredients <span className='text-red-500'>*</span>
                 </h1>
                 <button
                   className='dark flex w-16 items-center justify-between rounded-full px-2 dark:bg-primary-light dark:text-primary-dark'
                   type='button'
-                  onClick={() => directionsAppend({ direction: '' })}
+                  onClick={() =>
+                    ingredientAppend({ name: '', measurement: '', amount: 0 })
+                  }
                 >
                   <p>Add</p>
                   <PlusIcon />
                 </button>
               </div>
+              <div className='flex dark:text-primary-light'>
+                <p className='w-full'>Name</p>
+                <p className='w-full'>Unit</p>
+                <p className='w-full'>Amount</p>
+              </div>
               <div className='flex flex-col gap-2'>
-                {directionsFields.map((field, idx) => (
+                {ingredientFields.map((field, idx) => (
                   <div className='flex gap-3' key={field.id}>
                     <input
-                      placeholder={`${idx + 1}.) Direction`}
+                      placeholder={`Olive oil`}
                       className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
-                      {...register(`directions.${idx}.direction`)}
+                      {...register(`ingredients.${idx}.name`)}
+                    />
+                    <input
+                      placeholder={`tablespoon`}
+                      className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
+                      {...register(`ingredients.${idx}.measurement`)}
+                    />
+                    <input
+                      placeholder={`2`}
+                      className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
+                      {...register(`ingredients.${idx}.amount`)}
                     />
                     <button
                       type='button'
-                      onClick={() => directionsRemove(idx)}
+                      onClick={() => ingredientRemove(idx)}
                       className='rounded bg-red-500'
                     >
                       <XIcon />
@@ -310,148 +277,183 @@ export default function AddEditForm({ recipeId, type }: Params) {
                   </div>
                 ))}
               </div>
+
+              {/* Direction section */}
+              <div className='mt-6'>
+                <div className='mt-1 flex items-center gap-5'>
+                  <h1 className='block text-lg font-semibold dark:text-primary-light'>
+                    Directions <span className='text-red-500'>*</span>
+                  </h1>
+                  <button
+                    className='dark flex w-16 items-center justify-between rounded-full px-2 dark:bg-primary-light dark:text-primary-dark'
+                    type='button'
+                    onClick={() => directionsAppend({ direction: '' })}
+                  >
+                    <p>Add</p>
+                    <PlusIcon />
+                  </button>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  {directionsFields.map((field, idx) => (
+                    <div className='flex gap-3' key={field.id}>
+                      <input
+                        placeholder={`${idx + 1}.) Direction`}
+                        className='h-10 w-full rounded bg-slate-100 px-3 text-xl placeholder:text-base'
+                        {...register(`directions.${idx}.direction`)}
+                      />
+                      <button
+                        type='button'
+                        onClick={() => directionsRemove(idx)}
+                        className='rounded bg-red-500'
+                      >
+                        <XIcon />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
-          <label
-            className='mt-4 block text-lg font-semibold dark:text-primary-light'
-            htmlFor='tags'
-          >
-            Tags (separated by comma){' '}
-            <span className='text-sm font-light text-red-400'>
-              {errors.name?.message}
-            </span>
-          </label>
-          <input
-            type='text'
-            className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
-            {...register('tags')}
-          />
-          {errors.servings?.message && <p>{errors.servings.message}</p>}
-          <label
-            className='block text-lg font-semibold dark:text-primary-light'
-            htmlFor='servings'
-          >
-            Servings
-            <span className='text-sm font-light text-red-400'>
-              {errors.servings?.message}
-            </span>
-          </label>
-          <input
-            className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
-            type='text'
-            {...register('servings')}
-          />
+            <label
+              className='mt-4 block text-lg font-semibold dark:text-primary-light'
+              htmlFor='tags'
+            >
+              Tags (separated by comma){' '}
+              <span className='text-sm font-light text-red-400'>
+                {errors.name?.message}
+              </span>
+            </label>
+            <input
+              type='text'
+              className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
+              {...register('tags')}
+            />
+            {errors.servings?.message && <p>{errors.servings.message}</p>}
+            <label
+              className='block text-lg font-semibold dark:text-primary-light'
+              htmlFor='servings'
+            >
+              Servings
+              <span className='text-sm font-light text-red-400'>
+                {errors.servings?.message}
+              </span>
+            </label>
+            <input
+              className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
+              type='text'
+              {...register('servings')}
+            />
 
-          <label
-            className='mt-4 block text-lg font-semibold dark:text-primary-light'
-            htmlFor='prep'
-          >
-            Prep Time (minutes)
-            <span className='text-sm font-light text-red-400'>
-              {errors.prepTime?.message}
-            </span>
-          </label>
-          <input
-            className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
-            type='text'
-            {...register('prepTime')}
-          />
+            <label
+              className='mt-4 block text-lg font-semibold dark:text-primary-light'
+              htmlFor='prep'
+            >
+              Prep Time (minutes)
+              <span className='text-sm font-light text-red-400'>
+                {errors.prepTime?.message}
+              </span>
+            </label>
+            <input
+              className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
+              type='text'
+              {...register('prepTime')}
+            />
 
-          <label
-            className='mt-4 block text-lg font-semibold dark:text-primary-light'
-            htmlFor='cook'
-          >
-            Cook Time (minutes)
-            <span className='text-sm font-light text-red-400'>
-              {errors.cookTime?.message}
-            </span>
-          </label>
-          <input
-            className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
-            type='text'
-            {...register('cookTime')}
-          />
+            <label
+              className='mt-4 block text-lg font-semibold dark:text-primary-light'
+              htmlFor='cook'
+            >
+              Cook Time (minutes)
+              <span className='text-sm font-light text-red-400'>
+                {errors.cookTime?.message}
+              </span>
+            </label>
+            <input
+              className='block h-10 w-full rounded bg-slate-100 px-3 text-xl'
+              type='text'
+              {...register('cookTime')}
+            />
 
-          <label
-            className='mt-4 block text-lg font-semibold dark:text-primary-light'
-            htmlFor='description'
-          >
-            Description{' '}
-            <span className='text-sm font-light text-red-400'>
-              {errors.description?.message}
-            </span>
-          </label>
-          <textarea
-            {...register('description')}
-            className='block h-36 w-full rounded bg-slate-100 px-3 text-xl'
-          />
+            <label
+              className='mt-4 block text-lg font-semibold dark:text-primary-light'
+              htmlFor='description'
+            >
+              Description{' '}
+              <span className='text-sm font-light text-red-400'>
+                {errors.description?.message}
+              </span>
+            </label>
+            <textarea
+              {...register('description')}
+              className='block h-36 w-full rounded bg-slate-100 px-3 text-xl'
+            />
 
-          <label
-            className='mt-4 block text-lg font-semibold dark:text-primary-light'
-            htmlFor='images'
-          >
-            Upload photo
-          </label>
-          <input
-            className='w-full'
-            type='file'
-            name='images'
-            id='images'
-            multiple
-            onChange={(e) =>
-              setImage(e.target.files ? e.target.files[0] : null)
-            }
-          />
+            <label
+              className='mt-4 block text-lg font-semibold dark:text-primary-light'
+              htmlFor='images'
+            >
+              Upload photo
+            </label>
+            <input
+              className='w-full'
+              type='file'
+              name='images'
+              id='images'
+              multiple
+              onChange={(e) =>
+                setImage(e.target.files ? e.target.files[0] : null)
+              }
+            />
 
-          {type === 'edit' ? (
-            <>
-              <button
-                type='submit'
-                className='mt-4 block w-full rounded bg-primary-dark px-4 py-2 text-lighter-light disabled:bg-gray-200 disabled:text-gray-400 dark:bg-primary-light dark:text-primary-dark'
-                disabled={uploadLoading || addDocLoading}
-              >
-                Update Recipe
-              </button>
-              <div
-                className={`mt-4 flex flex-row-reverse ${
-                  deleteBuffer ? 'gap-0' : 'gap-2'
-                }`}
-              >
+            {type === 'edit' ? (
+              <>
                 <button
-                  type='button'
-                  className='block w-full rounded bg-red-500 px-4 py-2 text-lighter-light disabled:bg-gray-200 disabled:text-gray-400'
+                  type='submit'
+                  className='mt-4 block w-full rounded bg-primary-dark px-4 py-2 text-lighter-light disabled:bg-gray-200 disabled:text-gray-400 dark:bg-primary-light dark:text-primary-dark'
                   disabled={uploadLoading || addDocLoading}
-                  onClick={handleDelete}
                 >
-                  {!deleteBuffer ? 'Are you sure?' : 'Delete Recipe'}
+                  Update Recipe
                 </button>
-                {!deleteBuffer && (
+                <div
+                  className={`mt-4 flex flex-row-reverse ${
+                    deleteBuffer ? 'gap-0' : 'gap-2'
+                  }`}
+                >
                   <button
                     type='button'
-                    className={` block w-full rounded px-4 py-2 text-lighter-light disabled:bg-gray-200 disabled:text-gray-400 dark:bg-darker-light dark:text-darker-dark`}
+                    className='block w-full rounded bg-red-500 px-4 py-2 text-lighter-light disabled:bg-gray-200 disabled:text-gray-400'
                     disabled={uploadLoading || addDocLoading}
-                    onClick={() => setDeleteBuffer(true)}
+                    onClick={handleDelete}
                   >
-                    Cancel Delete
+                    {!deleteBuffer ? 'Are you sure?' : 'Delete Recipe'}
                   </button>
-                )}
-              </div>
-            </>
-          ) : (
-            <>
-              {' '}
-              <button
-                type='submit'
-                className='my-6 block w-full rounded bg-primary-dark px-4 py-2 text-lighter-light dark:bg-primary-light dark:text-primary-dark'
-                disabled={uploadLoading}
-              >
-                Create
-              </button>
-            </>
-          )}
-        </form>
+                  {!deleteBuffer && (
+                    <button
+                      type='button'
+                      className={` block w-full rounded px-4 py-2 text-lighter-light disabled:bg-gray-200 disabled:text-gray-400 dark:bg-darker-light dark:text-darker-dark`}
+                      disabled={uploadLoading || addDocLoading}
+                      onClick={() => setDeleteBuffer(true)}
+                    >
+                      Cancel Delete
+                    </button>
+                  )}
+                </div>
+              </>
+            ) : (
+              <>
+                {' '}
+                <button
+                  type='submit'
+                  className='my-6 block w-full rounded bg-primary-dark px-4 py-2 text-lighter-light dark:bg-primary-light dark:text-primary-dark'
+                  disabled={uploadLoading}
+                >
+                  Create
+                </button>
+              </>
+            )}
+          </form>
+        </div>
+        <Toaster />
       </div>
-      <Toaster />
-    </div>
+    </>
   );
 }
