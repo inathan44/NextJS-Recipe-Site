1e1743bf0599ab7ef29179c051d0f83ed919a361
inathan44 <inathan44@yahoo.com>
2023-06-20 21:32:16 -0400

    Moved search into separate function

--- lib/getAllRecipes.tsx
+++ lib/getAllRecipes.tsx
@@ -1,41 +1,22 @@
 import { getDocs } from 'firebase/firestore';
 import { recipeRef } from '@/firebaseConfig';
 
-export default async function getAllRecipes(searchTerm?: string) {
+export default async function getAllRecipes() {
   const snapshot = await getDocs(recipeRef);
 
   const recipes: Recipe[] = [];
 
-  if (!searchTerm) {
-    snapshot.docs.forEach((doc) => {
-      recipes.push({
-        id: doc.id,
-        name: '',
-        ingredients: [],
-        directions: [],
-        image: '',
-        tags: [],
-        description: '',
-        ...doc.data(),
-      });
-    });
-    return recipes;
-  }
-
   snapshot.docs.forEach((doc) => {
-    if (doc.data().name.includes(searchTerm)) {
-      recipes.push({
-        id: doc.id,
-        name: '',
-        ingredients: [],
-        directions: [],
-        image: '',
-        tags: [],
-        description: '',
-        ...doc.data(),
-      });
-    }
-    return recipes;
+    recipes.push({
+      id: doc.id,
+      name: '',
+      ingredients: [],
+      directions: [],
+      image: '',
+      tags: [],
+      description: '',
+      ...doc.data(),
+    });
   });
   return recipes;
 }
---
+++ lib/searchFilterRecipes.tsx
@@ -0,0 +1,41 @@
+import { getDocs } from 'firebase/firestore';
+import { recipeRef } from '@/firebaseConfig';
+
+export default async function searchFilterRecipes(searchTerm?: string) {
+  const snapshot = await getDocs(recipeRef);
+
+  const recipes: Recipe[] = [];
+
+  if (!searchTerm) {
+    snapshot.docs.forEach((doc) => {
+      recipes.push({
+        id: doc.id,
+        name: '',
+        ingredients: [],
+        directions: [],
+        image: '',
+        tags: [],
+        description: '',
+        ...doc.data(),
+      });
+    });
+    return recipes;
+  }
+
+  snapshot.docs.forEach((doc) => {
+    if (doc.data().name.includes(searchTerm)) {
+      recipes.push({
+        id: doc.id,
+        name: '',
+        ingredients: [],
+        directions: [],
+        image: '',
+        tags: [],
+        description: '',
+        ...doc.data(),
+      });
+    }
+    return recipes;
+  });
+  return recipes;
+}
