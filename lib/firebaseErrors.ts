7e7c981d4e2e63fa13923e81197af1d55fe80dc4
inathan44 <inathan44@yahoo.com>
2023-06-24 16:37:44 -0400

    Permissions error

--- lib/firebaseErrors.ts
+++ lib/firebaseErrors.ts
@@ -4,4 +4,6 @@ export const FIREBASE_ERRORS = {
 
   'Firebase: Error (auth/user-not-found).': 'Invalid email or password',
   'Firebase: Error (auth/wrong-password).': 'Invalid email or password',
+  'FirebaseError: Missing or insufficient permissions.':
+    'You do not have permission for this action.',
 };
