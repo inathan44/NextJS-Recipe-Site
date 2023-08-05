4d0d1e003544769003a642b410051b3955031c6f
inathan44 <inathan44@yahoo.com>
2023-08-05 15:05:39 -0400

    UOM lists

---
+++ lib/unitsOfMeasurement.ts
@@ -0,0 +1,33 @@
+export const units = [
+  'grams',
+  'liters',
+  'cups',
+  'quarts',
+  'pints',
+  'tablespoons',
+  'teaspoons',
+  'pinch',
+  'milliliters',
+  'pounds',
+  'ounces',
+  'gallons',
+  'spoonfuls',
+  'packages',
+  'kilograms',
+];
+
+export const customizers = [
+  'sliced',
+  'diced',
+  'chopped',
+  'minced',
+  'grated',
+  'mashed',
+  'peeled',
+  'trimmed',
+  'pureed',
+  'crushed',
+];
+
+units.sort();
+customizers.sort();
